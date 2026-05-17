3d67105fcf96c2a3668dedff18901bd9cf2a92dd
rain <129401103+rainaku@users.noreply.github.com>
2026-05-17 13:34:14 +0700

    Quick actions

--- VClothes/Views/DashboardView.xaml.cs
+++ VClothes/Views/DashboardView.xaml.cs
@@ -1,4 +1,7 @@
+using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
+using VClothes.ViewModels;
 
 namespace VClothes.Views;
 
@@ -7,5 +10,18 @@ public partial class DashboardView : UserControl
     public DashboardView()
     {
         InitializeComponent();
+    }
+
+    private void QuickAction_Click(object sender, MouseButtonEventArgs e)
+    {
+        if (sender is FrameworkElement element && element.Tag is string viewName)
+        {
+            // Navigate via MainViewModel
+            var mainWindow = Window.GetWindow(this);
+            if (mainWindow?.DataContext is MainViewModel mainVm)
+            {
+                mainVm.NavigateCommand.Execute(viewName);
+            }
+        }
     }
 }
